188029cad7153df5764e4f2f1a0af84f7d259166
ioanav9 <89965766+ioanav9@users.noreply.github.com>
2026-05-08 16:16:42 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>hello</h1>;
+}
